4e0efab378a6fffce0a504da4c3bf2edfbae53d8
Jonathan Olayinka <jonathanolayinka05@gmail.com.com>
2024-09-09 08:20:03 +0100

    changed 2023 to 2024 on the FooterSection

--- korensi/src/components/FooterSection.tsx
+++ korensi/src/components/FooterSection.tsx
@@ -5,7 +5,7 @@ const FooterSection: React.FC = () => {
     <footer className="bg-[#FFFFFF] text-[#02020280]">
       <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
         <p className="text-base text-center">
-          &copy; 2023 Korensi. All rights reserved.
+          &copy; 2024 Korensi. All rights reserved.
         </p>
       </div>
     </footer>
